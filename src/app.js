a417050b5b5afbe5908160feba6b3b94edd96cc3
Luis cid pedrero <luiscidp1@gmail.com>
2025-03-07 10:58:07 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
-import "./style.css";
+let articulos = ["El", "Nuestro", "Un" , "tu"]
+let descripciones = ["mejor", "magnifico", "increíble","meravelluç" , "apocaliptico"];
+let cosas = ["Erenthehusky", "Realmadrid", "calçot", "amic" ,"gambita"];
+let terminaciones = [".com", ".net", ".cat", ".es" ,".org"];
 
+function generarDominios() {
+    let listaDominios = document.getElementById("lista-dominios");
+    listaDominios.innerHTML = ""; 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    for (let i = 0; i < 10; i++) { 
+        let articulo = articulos[Math.floor(Math.random() * articulos.length)];
+        let descripcion = descripciones[Math.floor(Math.random() * descripciones.length)];
+        let cosa = cosas[Math.floor(Math.random() * cosas.length)];
+        let terminacion = terminaciones[Math.floor(Math.random() * terminaciones.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+        let dominio = `${articulo}${descripcion}${cosa}${terminacion}`;
+        let itemLista = document.createElement("li");
+        itemLista.className = "list-group-item";
+        itemLista.textContent = dominio;
+        listaDominios.appendChild(itemLista);
+    }
+}
